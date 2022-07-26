e29422346b9f8a9fd94bdb85ee3320c962359103
Neto Daniel Zinga <danielnzinga5@gmail.com>
2022-07-26 02:12:04 +0200

    Definition des functions trafficLight() et nextIndex()

--- feu.js
+++ feu.js
@@ -3,4 +3,9 @@ const buttons = document.getElementById('buttons')
 let colorIndex = 0;
 let intervalId = null;
 
+const trafficLight = ( event ) => {
+    stopAutomatic();
+    turnOn[event.target.id]();
+}
 
+const nextIndex = () => colorIndex = colorIndex > 2 ? ++colorIndex : 0;
