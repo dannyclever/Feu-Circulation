ef9158df0abb2cfef5005bd1ff14ccbf66fc5a52
Neto Daniel Zinga <danielnzinga5@gmail.com>
2022-07-26 02:54:00 +0200

    rajoutée la variable interValId et recoit null comme initialization

--- feu.js
+++ feu.js
@@ -3,15 +3,15 @@ const buttons = document.getElementById('buttons')
 let colorIndex = 0;
 let intervalId = null;
 
-const trafficLight = ( event ) => {
+const trafficLight = (event) => {
     stopAutomatic();
     turnOn[event.target.id]();
 }
 
-const nextIndex = () => colorIndex = colorIndex > 2 ? ++colorIndex : 0;
+const nextIndex = () => colorIndex = colorIndex < 2 ? ++colorIndex : 0;
 
 const changeColor = () => {
-    const colors = ['red', 'yellow', 'green']
+    const colors = ['red','yellow','green']
     const color = colors[colorIndex];
     turnOn[color]();
     nextIndex();
@@ -25,7 +25,7 @@ const turnOn = {
     'red':       () => img.src = 'img/vermelho.png',
     'yellow':    () => img.src = 'img/amarelo.png',
     'green':     () => img.src = 'img/verde.png',
-    'automatic': () => intervalId = setInterval(changeColor, 1000)
+    'automatic': () => intervalId = setInterval(changeColor, 1000);
 }
 
 buttons.addEventListener('click', trafficLight);
