a6f0560e6bf0007c3a79c85b553f7a6b5061c27a
Neto Daniel Zinga <danielnzinga5@gmail.com>
2022-07-26 03:05:04 +0200

    ...

--- feu.js
+++ feu.js
@@ -1,9 +1,9 @@
-const img = document.getElementById('img')
-const buttons = document.getElementById('buttons')
+const img = document.getElementById( 'img' );
+const buttons = document.getElementById( 'buttons' );
 let colorIndex = 0;
 let intervalId = null;
 
-const trafficLight = (event) => {
+const trafficLight = ( event ) => {
     stopAutomatic();
     turnOn[event.target.id]();
 }
@@ -12,20 +12,20 @@ const nextIndex = () => colorIndex = colorIndex < 2 ? ++colorIndex : 0;
 
 const changeColor = () => {
     const colors = ['red','yellow','green']
-    const color = colors[colorIndex];
+    const color = colors[ colorIndex ];
     turnOn[color]();
     nextIndex();
 }
 
 const stopAutomatic = () => {
-    clearInterval (intervalId);
+    clearInterval ( intervalId );
 }
 
 const turnOn = {
-    'red':       () => img.src = 'img/vermelho.png',
-    'yellow':    () => img.src = 'img/amarelo.png',
-    'green':     () => img.src = 'img/verde.png',
-    'automatic': () => intervalId = setInterval(changeColor, 1000);
+    'red':      () => img.src = './img/vermelho.png',
+    'yellow':   () => img.src = './img/amarelo.png',
+    'green':    () => img.src = './img/verde.png',
+    'automatic': () => intervalId = setInterval( changeColor, 1000 )
 }
 
-buttons.addEventListener('click', trafficLight);
+buttons.addEventListener('click', trafficLight );
